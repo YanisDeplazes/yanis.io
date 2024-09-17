a13189f925bf96d8a291bce6d3f8f061da566abe
Yanis Deplazes <yanisdeplazes@gmail.com>
2024-09-17 13:16:52 +0200

    deploy
    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import events from './src/data/events.json'; // Ensure this path is correct
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://yanis.io',
+  output: 'static',
+});
